b2b90451f8ddae1cd1d41c1767f84d7703ca6bd7
martsDev <martssilvapro@gmail.com>
2022-08-17 14:31:08 -0300

    implements navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,73 @@
 import React from 'react';
+import styled from 'styled-components';
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined"
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <Container>
+        <Wrapper>
+            <Search>
+                <Input placeholder='Search' />
+                <SearchOutlinedIcon />
+            </Search>
+            <Button>
+                <AccountCircleOutlinedIcon />
+                SIGN IN
+            </Button>
+        </Wrapper>
+    </Container>
   );
 };
+
+const Container = styled.section`
+    position: sticky;
+    top: 0;
+    background-color: ${({theme}) => theme.bgLighter};
+    height: 56px;
+`;
+
+const Wrapper = styled.section`
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    height: 100%;
+    padding: 0 20px;
+    position: relative;
+`;
+
+const Search = styled.div`
+    width: 40%;
+    position: absolute;
+    left: 0;
+    right: 0;
+    margin: auto;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 5px;
+    border: 1px solid #ccc;
+    border-radius: 10px;
+`;
+
+const Input = styled.input`
+    border: none;
+    background-color: transparent;
+    width: 100%;
+    outline: 0;
+`;
+
+const Button = styled.button`
+    padding: 5px 15px;
+    background-color: transparent;
+    border: 1px solid #3ea6ff;
+    color: #3ea6ff;
+    border-radius: 3px;
+    font-weight: 500;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    gap: 5;
+`;
 
 export default Navbar;
