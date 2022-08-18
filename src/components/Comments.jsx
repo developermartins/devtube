1df6a3bb735f84a727ba053a61b0fb3b9a785fb2
martsDev <martssilvapro@gmail.com>
2022-08-18 16:27:37 -0300

    add comments component

---
+++ src/components/Comments.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Comments = () => {
+  return (
+    <Container>Comments</Container>
+  );
+};
+
+const Container = styled.section`
+
+`;
+
+export default Comments;
