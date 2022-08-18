7377d58a23ae8c826389f9fdf657243aa6ae612d
martsDev <martssilvapro@gmail.com>
2022-08-18 18:44:01 -0300

    implements login page

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -54,11 +54,12 @@ const Menu = ({ darkMode, setDarkMode }) => {
             <Hr />
             <Login>
                 Sign in to like videos, comment, and subscribe.
-            
-                <Button>
-                    <AccountCircleOutlinedIcon />
-                    SIGN IN
-                </Button>
+                <Link to="signin" style={{ textDecoration: "none" }}>
+                    <Button>
+                        <AccountCircleOutlinedIcon />
+                        SIGN IN
+                    </Button>
+                </Link>
             </Login>
             <Hr />
             <Title>BEST OF MARTSTUBE</Title>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
 import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
-import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined"
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
@@ -11,10 +13,12 @@ const Navbar = () => {
                 <Input placeholder='Search' />
                 <SearchOutlinedIcon />
             </Search>
-            <Button>
-                <AccountCircleOutlinedIcon />
-                SIGN IN
-            </Button>
+            <Link to="signin" style={{ textDecoration: "none" }}>
+                <Button>
+                    <AccountCircleOutlinedIcon />
+                    SIGN IN
+                </Button>
+            </Link>
         </Wrapper>
     </Container>
   );
