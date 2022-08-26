9a57df4bdf27837edcbfceafe04275caa1b3298b
martsDev <martssilvapro@gmail.com>
2022-08-26 17:49:07 -0300

    setup redux persist data

--- src/index.js
+++ src/index.js
@@ -2,16 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css'
 import App from './App';
-import store from './redux/store';
 
-import { Provider } from 'react-redux';
+import { persistor, store } from './redux/store';
+import { Provider } from "react-redux";
+import { PersistGate } from 'redux-persist/integration/react';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={ store }>
-      <App />
-    </Provider>
+      <Provider store= {store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
   </React.StrictMode>
 );
