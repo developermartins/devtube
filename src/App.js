b1c4a1fbe8f256e0c9cf63e5d68663f0bb120f6a
martsDev <martssilvapro@gmail.com>
2022-08-26 10:47:57 -0300

    add type prop on route

--- src/App.js
+++ src/App.js
@@ -30,7 +30,9 @@ const App = () => {
             <Wrapper>
               <Routes>
                 <Route path='/'>
-                  <Route index element={ <Home /> } />
+                  <Route index element={ <Home type="random" /> } />
+                  <Route path="trends" element={ <Home type="trend" /> } />
+                  <Route path="subscriptions" element={ <Home type="sub" /> } />
                   <Route path='signin' element={ <Login /> } />
                   <Route path='video'>
                     <Route path=':id' element={ <Video /> } />
