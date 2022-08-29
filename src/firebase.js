65201e921a4a0847f86b1f4297b4d3a22f10436f
martsDev <martssilvapro@gmail.com>
2022-08-29 16:38:16 -0300

    add initial firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_FIREBASE_API_KEY,
@@ -10,3 +11,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth();
+export const provider = new GoogleAuthProvider();
+export default app;
