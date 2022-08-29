f5b277c1fa9de9f1913b1d7d856c791deaad5aac
martsDev <martssilvapro@gmail.com>
2022-08-29 16:34:59 -0300

    add initial firebase config

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_FIREBASE_API_KEY,
+  authDomain: "devtube-6c9f6.firebaseapp.com",
+  projectId: "devtube-6c9f6",
+  storageBucket: "devtube-6c9f6.appspot.com",
+  messagingSenderId: "316605514652",
+  appId: "1:316605514652:web:f38286654a4a4a099abe59"
+};
+
+const app = initializeApp(firebaseConfig);
