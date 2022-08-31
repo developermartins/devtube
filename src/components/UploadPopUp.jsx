bced3b52c2fa8a746c215710585d1624f0305248
martsDev <martssilvapro@gmail.com>
2022-08-31 15:36:06 -0300

    implments firebase upload

--- src/components/UploadPopUp.jsx
+++ src/components/UploadPopUp.jsx
@@ -1,22 +1,99 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+
+import app from '../firebase';
 import styled from 'styled-components';
 
 const UploadPopUp = ({ setOpen }) => {
+
+    const [img, setImg] = useState(undefined);
+    const [video, setVideo] = useState(undefined);
+    const [imgPercentage, setImgPercentage] = useState(0);
+    const [videoPercentage, setVideoPercentage] = useState(0);
+    const [inputs, setInputs] = useState({});
+    const [tags, setTags] = useState([]);
+
+    const handleTags = (e) => {
+        setTags(e.target.value.split(","));
+    };
+
+    const handlechange = (e) => {
+        setInputs((prev) => {
+            return {
+                ...prev,
+                [e.target.name]: e.target.value,
+            };
+        });
+    };
+
+    const uploadFile = (file, urlType) => {
+        const storage = getStorage(app);
+        const fileName = new Date().getTime() + file.name;
+        const storageRef = ref(storage, fileName);
+        const uploadTask = uploadBytesResumable(storageRef, file);
+
+        uploadTask.on('state_changed',
+            (snapshot) => {
+                // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+                const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                urlType === "imgUrl" ? setImgPercentage(Math.round(progress)) : setVideoPercentage(Math.round(progress));
+                switch (snapshot.state) {
+                case 'paused':
+                    console.log('Upload is paused');
+                    break;
+                case 'running':
+                    console.log('Upload is running');
+                    break;
+                default:
+                    break;
+                };
+            },
+            (error) => {},
+            () => {
+                // Upload completed successfully, now we can get the download URL
+                getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                    setInputs((prev) => {
+                        return {
+                            ...prev,
+                            [urlType]: downloadURL,
+                        };
+                    });
+                });
+            },
+        );
+    };
+
+    useEffect(() => {
+       video && uploadFile(video, "videoUrl");
+    }, [video]);
+
+    useEffect(() => {
+       img && uploadFile(img, "imgUrl");
+    }, [img]);
+
   return (
     <Container>
         <Wrapper>
             <Close onClick={ () => setOpen(false) }>X</Close>
             <Title>Upload a New Video</Title>
             <Label>Video:</Label>
-            <Input type="file" accept="video/*" />
+            {  
+                videoPercentage > 0 ? ("Uploading: " + videoPercentage + "%") : (
+                    <Input type="file" accept="video/*" onChange={ e => setVideo(e.target.files[0]) } />
+                )
+            }
             <Label>Title:</Label>
-            <Input type="text" placeholder='Video Title' />
+            <Input type="text" placeholder='Video Title' name='title' onChange={ handlechange } />
             <Label>Description:</Label>
-            <Description placeholder="Add a description" rows={ 8 } />
+            <Description placeholder="Add a description" name='description' rows={ 8 }  onChange={ handlechange } />
             <Label>Tags:</Label>
-            <Input type="text" placeholder='Separate the tags with commas.' />
+            <Input type="text" placeholder='Separate the tags with commas.' onChange={ handleTags } />
             <Label>Video thumb:</Label>
-            <Input type="file" accept="image/*" />
+            {
+                imgPercentage > 0 ? ("Uploading: " + imgPercentage + "%") : (
+                    <Input type="file" accept="image/*" onChange={ e => setImg(e.target.files[0]) } />
+                )
+            }
             <UploadButton>Upload</UploadButton>
         </Wrapper> 
     </Container>
