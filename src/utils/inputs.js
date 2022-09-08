65b2896ea1edd7ca9625aebbc74dc83f6919440e
martsDev <martssilvapro@gmail.com>
2022-09-08 14:39:46 -0300

    implements inputs array

--- src/utils/inputs.js
+++ src/utils/inputs.js
@@ -5,5 +5,26 @@ export default inputs = [
         type: "text",
         placeholder: "Username",
         label: "Username",
-    }
+    },
+    {
+        id: 2,
+        name: "email",
+        type: "text",
+        placeholder: "Email",
+        label: "Email",
+    },
+    {
+        id: 3,
+        name: "password",
+        type: "password",
+        placeholder: "Password",
+        label: "Password",
+    },
+    {
+        id: 4,
+        name: "confirm-password",
+        type: "password",
+        placeholder: "Confirm password",
+        label: "Confirm password",
+    },
 ];
