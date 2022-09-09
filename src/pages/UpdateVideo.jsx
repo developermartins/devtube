f6272b162f5222c96943dcc34c406a5db255d04c
martsDev <martssilvapro@gmail.com>
2022-09-09 10:36:43 -0300

    implements update video page

--- src/pages/UpdateVideo.jsx
+++ src/pages/UpdateVideo.jsx
@@ -1,32 +1,87 @@
 import React, { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
-import { fetchVideoById } from '../services/fetchVideos';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { useSelector } from 'react-redux';
 
 import styled from 'styled-components';
 import Card from '../components/Card';
-import { useSelector } from 'react-redux';
-
+import app from '../firebase';
 
 const UpdateVideo = () => {
 
     const path = useLocation().pathname.split("/")[2];
 
-    const [video, setVideo] = useState({});
-    const [img, setImg] = useState(undefined);
-    const [imgPercentage, setImgPercentage] = useState(0);
-
-    // useEffect(() => {
-
-    //     const fetchVideo = async () => {
-    //         const currentVideo = await fetchVideoById(path.toString());
-    //         setVideo(currentVideo); 
-    //     };
-    //     fetchVideo();
-    // }, []);
-
-    // console.log(video)
+    const navigate = useNavigate();
 
     const { currentVideo } = useSelector((state) => state.video);
+
+    const [img, setImg] = useState(undefined);
+    const [imgPercentage, setImgPercentage] = useState(0);
+    const [inputs, setInputs] = useState({});
+    const [tags, setTags] = useState([]);
+
+    const handleTags = (e) => {
+        setTags(e.target.value.split(","));
+    };
+
+    const handlechange = (e) => {
+        setInputs((prev) => {
+            return {
+                ...prev,
+                [e.target.name]: e.target.value,
+            };
+        });
+    };
+
+    const uploadFile = (file, urlType) => {
+        const storage = getStorage(app);
+        const fileName = new Date().getTime() + file.name;
+        const storageRef = ref(storage, fileName);
+        const uploadTask = uploadBytesResumable(storageRef, file);
+
+        uploadTask.on('state_changed',
+            (snapshot) => {
+                // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+                const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                urlType === "imgUrl" && setImgPercentage(Math.round(progress));
+                switch (snapshot.state) {
+                case 'paused':
+                    console.log('Upload is paused');
+                    break;
+                case 'running':
+                    console.log('Upload is running');
+                    break;
+                default:
+                    break;
+                };
+            },
+            (error) => {},
+            () => {
+                // Upload completed successfully, now we can get the download URL
+                getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                    setInputs((prev) => {
+                        return {
+                            ...prev,
+                            [urlType]: downloadURL,
+                        };
+                    });
+                });
+            },
+        );
+    };
+
+    useEffect(() => {
+        img && uploadFile(img, "imgUrl");
+    }, [img]);
+
+
+    const handleUpdate = async (e) => {
+        e.preventDefault();
+
+        
+
+        // res.status === 201 && navigate(`/video/${res.data._id}`);
+    };
 
     console.log(currentVideo)
 
@@ -39,17 +94,21 @@ const UpdateVideo = () => {
         </Section>
         <FormSection>
             <Label>Title:</Label>
-            <Input type="text" placeholder='Video Title' name='title' />
+            <Input type="text" placeholder='Video Title' name='title' onChange={ handlechange } />
             <Label>Description:</Label>
-            <Description placeholder="Add a description" name='description' rows={ 8 }   />
+            <Description placeholder="Add a description" name='description' rows={ 8 } onChange={ handlechange } />
             <Label>Tags:</Label>
-            <Input type="text" placeholder='Separate the tags with commas.'  />
+            <Input type="text" placeholder='Separate the tags with commas.' onChange={ handleTags } />
             <Label>Video thumb:</Label>
             {
                 imgPercentage > 0 ? ("Uploading: " + imgPercentage + "%") : (
                     <Input type="file" accept="image/*" onChange={ e => setImg(e.target.files[0]) } />
                 )
             }
+           <ButtonsSection>
+                <UpdateVideoBtn>Update video</UpdateVideoBtn>
+                <DeleteVideoBtn>Delete video</DeleteVideoBtn>
+           </ButtonsSection>
         </FormSection>
     </Container>
   );
@@ -78,7 +137,6 @@ const Hr = styled.hr`
 `;
 
 const FormSection = styled.section`
-    border: 1px solid red;
     display: flex;
     flex-direction: column;
     padding: 20px;
@@ -107,6 +165,36 @@ const Description = styled.textarea`
     background-color: transparent;
     width: 50%;
     margin-bottom: 15px;
+`;
+
+const ButtonsSection = styled.section`
+    display: flex;
+    margin-top: 25px;
+`;
+
+const UpdateVideoBtn = styled.button`
+    background-color: #007dd6;
+    font-weight: 500;
+    color: white;
+    border: none;
+    border-radius: 3px;
+    height: max-content;
+    padding: 10px 20px;
+    cursor: pointer;
+    text-transform: uppercase;
+    margin-right: 30px;
+`;
+
+const DeleteVideoBtn = styled.button`
+    background-color: #cc1a00;
+    font-weight: 500;
+    color: white;
+    border: none;
+    border-radius: 3px;
+    height: max-content;
+    padding: 10px 20px;
+    cursor: pointer;
+    text-transform: uppercase;
 `;
 
 export default UpdateVideo;
